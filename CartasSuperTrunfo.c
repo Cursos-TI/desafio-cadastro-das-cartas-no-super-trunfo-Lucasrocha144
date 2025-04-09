6ca8fad05086a324d5a376739bd06833dc8ebd87
Lucasrocha144 <lucasrochalr144@hotmail.com>
2025-04-09 23:27:39 +0000

    cartassupertrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,13 +8,16 @@ int main() {
     printf("Desafio Super Trunfo: Cadastrar duas cartas\n");
     printf("Cadastro da carta 1/2: \n");
 
-    char estado[50];
-    char codigo[5];
-    char nome[20];
-    int população;
-    float área;
-    float pib;
-    float numero;
+    char estado = A;
+    char codigo = A01;
+    char nome = São Paulo;
+    int população = 123.250;
+    float área = 123.250 km²;
+    float pib =  699.28 bilhões de reais;
+    float numero de pontos turistico = 50;
+    float densidade populacional: 8102.47 hab/km²;
+    float pib por capita = 56724.32 reais;
+
 
     printf("Digite a inicial do nome do estado:\n");
     scanf("%s", &Estado);
@@ -37,17 +40,27 @@ int main() {
     printf("Numero de pontos turisticos:\n");
     scanf("%i", &numero);
 
-    printf("Carta 1/2:\n o nome do Estado é :%s - o código da cidade é:%s - O nome da cidade é:%s - Total da população é:%i - O tamanho da área é:%f - O PIB é:%f - Total de pontos turistico :%i \n\n",  Estado, Codigo, Nome, Populaçao, Area, PIB, Numero);
+    printf(Densidade populacional:\n);
+    scanf("%f", &densidade);
+
+    printf("Pib per capita:\n");
+    scanf("%f", &pib per capita);
+    
+    printf("O nome do estado é:%s - O código da cidade é:%s - Digite o nome da cidade:%s - Total da população:%f - O tamanho da área é:%f -O pib é:%f - Total de pontos turistico é:%i - A densidade populacional é:%f - O pib por capita é:%f \n\n", estado, código, cidade,  população, área, pib, numero, densidade populacional, pib por capita);
 
     printf("Cadastro da carta 2/2\n");
 
-    char estado[50];
-    char codigo[5];
-    char cidade[50];
-    int populaçao;
-    float area;
-    float pib;
-    float numero;
+    char estado = B;
+    char codigo = B02;
+    char cidade = Rio de Janeiro;
+    int populaçao = 674.8000
+    float area = 1200.25 km²;
+    float pib = 300.50 bilhões de reais;
+    float numero de pontos turistico = 30;
+   float  densidade populacional = 5622.24 hab/km²;
+    float pib per capita = 44532.91 reais;
+
+
 
     printf("Digite a inicial do nome do estado:\n");
     scanf("%s", &estado);
@@ -70,9 +83,14 @@ int main() {
     printf("Número de pontos turistico:\n");
     scanf("%i", &numero);
 
-    printf("O nome do estado é:%s - O código da cidade é:%s - Digite o nome da cidade:%s - Total da população:%f - O tamanho da área é:%f - O PIB é:%f - Total de pontos turisticos é:%i \n\n", estado, código, cidade, populaçao, área, pib, numero);
-        return 0;
+    printf("Densidade populacional:\n");
+    scanf("%f", &densidade);
 
+    printf("pib per capit:\n");
+    scanf("%f", &pib);
+
+    printf("O nome do estado é:%s - O código da cidade é:%s - Digite o nome da cidade:%s - Total de população:%f - O tamanho da área é:%f - O pib é:%f - Total de pontos turisticos:%i - a Densidade populacional é:%f - O pib per capita é:%f \n\n", estado, código, cidade, população, área, pib, numero, densidade, pib);
+    
 
 
 }
